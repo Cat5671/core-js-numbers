063df4c117fbafe00a11eea174b7a834507de754
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2024-12-05 19:20:01 +0300

    feat: add 4 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -69,7 +69,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  throw new Error('Not implemented');
+  return Math.sqrt(Math.pow((Math.max(x1, x2) - Math.min(x1, x2)), 2) + Math.pow((Math.max(y1, y2) - Math.min(y1, y2)), 2));
 }
 
 /**
