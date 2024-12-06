ec4cf5ce4eb9dd2d8070071434c1a34e8fae4cf9
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2024-12-06 18:29:28 +0300

    feat: add 5 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -7,67 +7,18 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-/**
- * Returns an area of a rectangle given by width and height.
- *
- * @param {number} width
- * @param {number} height
- * @return {number}
- *
- * @example:
- *   5, 10 => 50
- *   5, 5  => 25
- */
 function getRectangleArea(width, height) {
   return width * height;
 }
 
-/**
- * Returns a circumference of circle given by radius.
- *
- * @param {number} radius
- * @return {number}
- *
- * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
- */
 function getCircleCircumference(radius) {
   return radius * 2 * Math.PI;
 }
 
-/**
- * Returns an average of two given numbers.
- *
- * @param {number} value1
- * @param {number} value2
- * @return {number}
- *
- * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
- */
 function getAverage(value1, value2) {
   return (value1 + value2) / 2;  
 }
 
-/**
- * Returns a distance between two points by cartesian coordinates.
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- *
- * @return {number}
- *
- * @example:
- *   (0,0) (0,1)    => 1
- *   (0,0) (1,0)    => 1
- *   (-5,0) (10,-10) => 18.027756377319946
- */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow((Math.max(x1, x2) - Math.min(x1, x2)), 2) + Math.pow((Math.max(y1, y2) - Math.min(y1, y2)), 2));
 }
@@ -84,8 +35,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return ((-1) * b) / a;
 }
 
 /**
