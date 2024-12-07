accac7e13ba19797e8759f263d37c7d503657f2c
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2024-12-07 10:49:30 +0300

    feat: add task6

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -1,21 +1,26 @@
 function getRectangleArea(width, height) {
-  return width * height;
+    'use strict';
+    return width * height;
 }
 
 function getCircleCircumference(radius) {
-  return radius * 2 * Math.PI;
+    'use strict';
+    return radius * 2 * Math.PI;
 }
 
 function getAverage(value1, value2) {
-  return (value1 + value2) / 2;  
+    'use strict';
+    return (value1 + value2) / 2;
 }
 
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow((Math.max(x1, x2) - Math.min(x1, x2)), 2) + Math.pow((Math.max(y1, y2) - Math.min(y1, y2)), 2));
+    'use strict';
+    return Math.sqrt(Math.pow((Math.max(x1, x2) - Math.min(x1, x2)), 2) + Math.pow((Math.max(y1, y2) - Math.min(y1, y2)), 2));
 }
 
 function getLinearEquationRoot(a, b) {
-  return ((-1) * b) / a;
+    'use strict';
+    return ((-1) * b) / a;
 }
 
 /**
