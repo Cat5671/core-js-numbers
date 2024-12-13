75579fbfd159cc6057ec3723a0c03da02e017216
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2024-12-13 13:53:14 +0300

    feat: add task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -23,7 +23,9 @@ function getLinearEquationRoot(a, b) {
 }
 
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return (x1 * x2 + y1 * y2) / Math.sqrt(x1 ** 2 + y1 ** 2) / Math.sqrt(x2 ** 2 + y2 ** 2);
+  const scalarProduct = x1 * x2 + y1 * y2;
+  const length1 = Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2);
+  return scalarProduct / length1;
 }
 
 function getLastDigit(value) {
